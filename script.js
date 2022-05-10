ab56094287f7b106630c45e15f9d114121185d4f
Greg Brosman <greg.brosman@gmail.com>
2022-05-09 18:23:24 -0700

    Form labels added. JS added to calculate percentages.

--- script.js
+++ script.js
@@ -10,8 +10,24 @@ function calc_max(weight_lifted, reps_lifted) {
         const max = Math.round(Number(weight_lifted + result));
         const max_weight_result = document.getElementById('max_weight');
 
-        return max_weight_result.innerHTML = max ;
+        calc_percentages(max);
+
+        return max_weight_result.innerHTML = max;
 };
+
+function calc_percentages(max) {
+    const percent90 = Math.round(max * .90);
+    const percent80 = Math.round(max * .80);
+    const percent70 = Math.round(max * .70);
+
+    const resultpercent90 = document.getElementById("percent90");
+    const resultpercent80 = document.getElementById("percent80");
+    const resultpercent70 = document.getElementById("percent70");
+
+
+
+    return resultpercent90.innerHTML = percent90, resultpercent80.innerHTML = percent80, resultpercent70.innerHTML = percent70;
+}
 
 
 calculator.addEventListener("submit", function(e) {
@@ -27,6 +43,9 @@ calculator.addEventListener("submit", function(e) {
 
 // Reset button clears to 0
 calculator.addEventListener("reset", function() {
-    return document.getElementById("max_weight").innerHTML = 0;
+    return document.getElementById("max_weight").innerHTML = 0, 
+    document.getElementById("percent90").innerHTML = 0, 
+    document.getElementById("percent80").innerHTML = 0, 
+    document.getElementById("percent70").innerHTML = 0;
 
 });
